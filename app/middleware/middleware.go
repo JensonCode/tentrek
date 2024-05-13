5c27fc412431f4ccc35db17769d31c8ac8445ed6
JensonCode <lclcoding0714@hotmail.com>
2024-05-13 18:05:32 -0400

    refactor api cors middleware

--- app/middleware/middleware.go
+++ app/middleware/middleware.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"net/http"
-	"os"
 )
 
 type Middleware func(http.HandlerFunc) http.HandlerFunc
@@ -14,22 +13,4 @@ func Chain(h http.HandlerFunc, mws ...Middleware) http.HandlerFunc {
 	}
 
 	return h
-}
-
-func WithCors(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-
-		allowedOrigin := os.Getenv("APP_BASE_URL")
-
-		w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		h(w, r)
-	}
 }
